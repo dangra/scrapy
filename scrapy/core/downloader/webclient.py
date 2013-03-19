70cc2b09e8de7a12c650fc743add596e15477309
Daniel Graña <dangra@gmail.com>
2013-03-19 18:58:16 -0300

    rework a bit of proxy https support
    - use mixedCaps only for twisted events
    - Do not send Proxy-* headers after tunneling
    - Send Proxy-Authorization on CONNECT

--- scrapy/core/downloader/webclient.py
+++ scrapy/core/downloader/webclient.py
@@ -33,87 +33,96 @@ def _parse(url):
 class ScrapyHTTPPageGetter(HTTPClient):
 
     delimiter = '\n'
+    tunnel_started = False
+    tunnel_headers = ['Host', 'Proxy-Connection', 'Proxy-Authorization', 'User-Agent']
 
     def connectionMade(self):
-        self.headers = Headers() # bucket for response headers
-
+        # bucket for response headers
+        self.headers = Headers()
         if self.factory.use_tunnel:
-            log.msg("Sending CONNECT", log.DEBUG)
-            self.tunnel_started = False
-            self.sendCommand("CONNECT", "%s:%s"
-                % (self.factory.tunnel_to_host, self.factory.tunnel_to_port))
-            self.sendHeaders(only=['Host','Proxy-Connection', 'User-Agent'])
-            del self.factory.headers['Proxy-Connection']
+            hp = "%s:%s" % (self.factory.tunnel_to_host,
+                            self.factory.tunnel_to_port)
+            log.msg("Sending CONNECT %s" % hp, log.DEBUG)
+            self._send_command('CONNECT', hp)
+            self._send_tunnel_headers()
         else:
-            self.sendEverything()
+            self._send_everything()
 
-
-    def sendCommand(self, command, path):
+    def _send_command(self, command, path):
         if self.factory.use_tunnel and not self.tunnel_started:
             http_version = "1.1"
         else:
             http_version = "1.0"
-        self.transport.write('%s %s HTTP/%s\r\n' % (command, path, http_version))
+        cmdline = '%s %s HTTP/%s\r\n' % (command, path, http_version)
+        log.msg('Sending command: %r' % cmdline)
+        self.transport.write(cmdline)
 
-    def sendEverything(self):
-        self.sendMethod()
-        self.sendHeaders()
-        self.sendBody()
+    def _send_everything(self):
+        self._send_method()
+        self._send_headers()
+        self._send_body()
 
-    def sendMethod(self):
-        # Method command
-        self.sendCommand(self.factory.method, self.factory.path)
+    def _send_method(self):
+        self._send_command(self.factory.method, self.factory.path)
 
-    def sendHeaders(self, only=None):
-        # Note: it's a Headers object, not a dict
-        keys = only if only is not None else self.factory.headers.keys()
-        for key in keys:
-            for value in self.factory.headers.getlist(key):
-                self.sendHeader(key, value)
+    def _send_single_header(self, key):
+        for value in self.factory.headers.getlist(key):
+            log.msg('Sending header %s: %s' % (key, value))
+            self.sendHeader(key, value)
+
+    def _send_tunnel_headers(self):
+        for key in self.tunnel_headers:
+            self._send_single_header(key)
         self.endHeaders()
 
-    def sendBody(self):
-        # Body
+    def _send_headers(self):
+        for key in self.factory.headers.keys():
+            # Skip proxy headers in case of tunneling with CONNECT method
+            if self.factory.use_tunnel and key.startswith('Proxy-'):
+                continue
+            self._send_single_header(key)
+        self.endHeaders()
+
+    def _send_body(self):
         if self.factory.body is not None:
             self.transport.write(self.factory.body)
 
     def lineReceived(self, line):
-        if self.factory.use_tunnel and not self.tunnel_started: log.msg("LINE: %s" % line)
+        if self.factory.use_tunnel and not self.tunnel_started:
+            log.msg("LINE: %s" % line)
+
         if self.factory.use_tunnel and not self.tunnel_started and not line.rstrip():
             # End of headers from the proxy in response to our CONNECT request
             # Skip the call to HTTPClient.lienReceived for now, since otherwise
             # it would switch to row mode.
-            self.startTunnel()
+            self._start_tunnel()
         else:
             return HTTPClient.lineReceived(self, line.rstrip())
 
-    def startTunnel(self):
+    def _start_tunnel(self):
         log.msg("starting Tunnel")
-
         # We'll get a new batch of headers through the tunnel. This sets us
         # up to capture them.
         self.firstLine = True
         self.tunnel_started = True
-
         # Switch to SSL
         ctx = ClientContextFactory()
         self.transport.startTLS(ctx, self.factory)
-
         # And send the normal request:
-        self.sendEverything()
-
+        self._send_everything()
 
     def handleHeader(self, key, value):
         if self.factory.use_tunnel and not self.tunnel_started:
-             pass # maybe log headers for CONNECT request?
+            log.msg('Proxy header "%s: %s"' % (key, value))
+            pass  # maybe log headers for CONNECT request?
         else:
-             self.headers.appendlist(key, value)
+            self.headers.appendlist(key, value)
 
     def handleStatus(self, version, status, message):
         if self.factory.use_tunnel and not self.tunnel_started:
-             self.tunnel_status = status
+            self.tunnel_status = status
         else:
-             self.factory.gotStatus(version, status, message)
+            self.factory.gotStatus(version, status, message)
 
     def handleEndHeaders(self):
         self.factory.gotHeaders(self.headers)
@@ -131,9 +140,9 @@ class ScrapyHTTPPageGetter(HTTPClient):
 
     def timeout(self):
         self.transport.loseConnection()
-        self.factory.noPage(\
-                defer.TimeoutError("Getting %s took longer than %s seconds." % \
-                (self.factory.url, self.factory.timeout)))
+        msg = "Getting %s took longer than %s seconds." % (self.factory.url,
+                                                           self.factory.timeout)
+        self.factory.noPage(defer.TimeoutError(msg))
 
 
 class ScrapyHTTPClientFactory(HTTPClientFactory):
@@ -178,7 +187,7 @@ class ScrapyHTTPClientFactory(HTTPClientFactory):
             self.headers.setdefault("Connection", "close")
 
     def _build_response(self, body, request):
-        request.meta['download_latency'] = self.headers_time-self.start_time
+        request.meta['download_latency'] = self.headers_time - self.start_time
         status = int(self.status)
         headers = Headers(self.response_headers)
         respcls = responsetypes.from_args(headers=headers, url=self.url)
@@ -192,17 +201,17 @@ class ScrapyHTTPClientFactory(HTTPClientFactory):
         if proxy:
             old_scheme, old_host, old_port = self.scheme, self.host, self.port
             self.scheme, _, self.host, self.port, _ = _parse(proxy)
-            if old_scheme=="https" and 'ssl' in optional_features:
-                 self.headers['Proxy-Connection'] = 'keep-alive'
-                 self.use_tunnel = True
-                 self.tunnel_to_host = old_host
-                 self.tunnel_to_port = old_port
-            if not self.use_tunnel: self.path = self.url
+            if old_scheme == "https":
+                self.use_tunnel = True
+                self.tunnel_to_host = old_host
+                self.tunnel_to_port = old_port
+                self.headers['Proxy-Connection'] = 'keep-alive'
+            else:
+                self.path = self.url
 
     def gotHeaders(self, headers):
         self.headers_time = time()
         self.response_headers = headers
-
 
 
 if 'ssl' in optional_features:
